bfb1a3e6bebbbe873675737d5414f5f280377b64
Irene <ireneog_72@hotmail.es>
2019-12-30 10:38:44 +0100

    initial commit

---
+++ models/Usuario.php
@@ -0,0 +1,58 @@
+<?php 
+
+class Usuario{
+  public $nombre;
+  public $apellidos;
+  public $email;
+  public $password;
+
+  public function getNombre()
+  {
+    return $this->nombre;
+  }
+
+  public function setNombre($nombre)
+  {
+    $this->nombre = $nombre;
+
+    //return $this;
+  }
+
+  public function getApellidos()
+  {
+    return $this->apellidos;
+  }
+
+  public function setApellidos($apellidos)
+  {
+    $this->apellidos = $apellidos;
+
+    //return $this;
+  }
+
+  public function getEmail()
+  {
+    return $this->email;
+  }
+
+  public function setEmail($email)
+  {
+    $this->email = $email;
+
+    //return $this;
+  }
+
+  public function getPassword()
+  {
+    return $this->password;
+  }
+
+  public function setPassword($password)
+  {
+    $this->password = $password;
+
+    //return $this;
+  }
+}
+
+?>
