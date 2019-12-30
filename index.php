837e348485251e3542081c7f805283b35a0db9ea
Irene <ireneog_72@hotmail.es>
2019-12-30 11:29:16 +0100

    sustitucion index estatico por Cotrolador Frontal

--- index.php
+++ index.php
@@ -1,22 +1,27 @@
 <!--<h1>Hola Mundo MVC!</h1>-->
 <h1>Bienvenido a mi web con MVC</h1>
 <?php
+  //creación de un Controlador Frontal: se encarga de cargar un fichero y una acción u otra en función de lo que viene por la url
   require_once 'controllers/UsuarioController.php';
 
-  $controlador = new UsuarioController();
-  $controlador->mostrarTodos();
-  $controlador->crear();
+  if( isset($_GET['controller']) && class_exists( $_GET['controller'] ) ){
+  
+    $nombre_controlador = $_GET['controller'];
+    $controlador = new $nombre_controlador();
+      //$controlador->mostrarTodos();
+      //$controlador->crear();
 
-  if( isset($_GET['action']) && method_exists( $controlador, $_GET['action']) ){
-    $action = $_GET['action'];
-
-    $controlador->$action();
-
+    if( isset($_GET['action']) && method_exists( $controlador, $_GET['action']) ){
+      $action = $_GET['action'];
+  
+      $controlador->$action();
+    }else{
+      echo "La página que buscas no existe";
+    }
+    
   }else{
     echo "La página que buscas no existe";
   }
-  
-  
 
 
 
