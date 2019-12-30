d5d2b736c7101d748a930e963532df8b1fcc649a
Irene <ireneog_72@hotmail.es>
2019-12-30 11:41:25 +0100

    improvement the url to cut text lenght

--- index.php
+++ index.php
@@ -4,19 +4,22 @@
   //creación de un Controlador Frontal: se encarga de cargar un fichero y una acción u otra en función de lo que viene por la url
   require_once 'controllers/UsuarioController.php';
 
-  if( isset($_GET['controller']) && class_exists( $_GET['controller'] ) ){
+  if( isset($_GET['controller']) ){
   
-    $nombre_controlador = $_GET['controller'];
-    $controlador = new $nombre_controlador();
+    $nombre_controlador = $_GET['controller'] . 'Controller';
+
+    if( class_exists( $nombre_controlador ) ){
+      $controlador = new $nombre_controlador();
       //$controlador->mostrarTodos();
       //$controlador->crear();
 
-    if( isset($_GET['action']) && method_exists( $controlador, $_GET['action']) ){
-      $action = $_GET['action'];
-  
-      $controlador->$action();
-    }else{
-      echo "La página que buscas no existe";
+      if( isset($_GET['action']) && method_exists( $controlador, $_GET['action']) ){
+        $action = $_GET['action'];
+    
+        $controlador->$action();
+      }else{
+        echo "La página que buscas no existe";
+      }
     }
     
   }else{
